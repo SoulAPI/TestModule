e35e87f3c1257b7bb5e2dd3c7f7ba340139b05ed
unfamoussoul <ibudda.dict@gmail.com>
2026-02-28 19:43:48 +0400

    1.2.0 update

--- src/main/java/com/unfamoussoul/test/command/TestCommand.java
+++ src/main/java/com/unfamoussoul/test/command/TestCommand.java
@@ -1,20 +1,28 @@
 package com.unfamoussoul.test.command;
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.unfamoussoul.sapi.api.command.DynamicCommand;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import org.bukkit.command.CommandSender;
 
 public class TestCommand extends DynamicCommand {
 
     public TestCommand() {
-        super(
-                "test",
-                Commands.literal("test")
-                        .executes(context -> {
-                            CommandSender sender = context.getSource().getSender();
-                            sender.sendRichMessage(sender.getName());
-                            return 1;
-                        })
-        );
+        super("test");
+        initialize();
+    }
+
+    private void initialize() {
+        register(test());
+    }
+
+    private LiteralArgumentBuilder<CommandSourceStack> test() {
+        return Commands.literal("test")
+                .executes(context -> {
+                    CommandSender sender = context.getSource().getSender();
+                    sender.sendRichMessage(sender.getName());
+                    return 1;
+                });
     }
 }
